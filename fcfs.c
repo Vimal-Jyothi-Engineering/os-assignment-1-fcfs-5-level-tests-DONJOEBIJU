6d6886d3c1a241b87c3ff2c1af8e04e0a57b99e0
ALANTA MARIYA BABU <alantamariya.shibi65@gmail.com>
2026-03-03 23:48:07 +0530

    Added FCFS scheduling implementation by sorting processes based on arrival time and computing waiting time, turnaround time, total WT, total TAT, average WT, and average TAT.

--- fcfs.c
+++ fcfs.c
@@ -1,73 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Process {
-    int pid;
-    int at;
-    int bt;
-    int wt;
-    int tat;
-    int original_index;
-};
-
 int main() {
     int n;
     scanf("%d", &n);
 
-    struct Process p[100];
+    int pid[100], at[100], bt[100];
+    int wt[100], tat[100];
 
     for (int i = 0; i < n; i++) {
         char pname[20];
-        scanf("%s %d %d", pname, &p[i].at, &p[i].bt);
-        p[i].pid = atoi(pname + 1);
-        p[i].original_index = i;
+        scanf("%s %d %d", pname, &at[i], &bt[i]);
+        pid[i] = atoi(pname + 1);
     }
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].at > p[j + 1].at) {
-                struct Process temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+            if (at[j] > at[j + 1]) {
+                int t;
+                t = at[j];  at[j] = at[j+1];  at[j+1] = t;
+                t = bt[j];  bt[j] = bt[j+1];  bt[j+1] = t;
+                t = pid[j]; pid[j] = pid[j+1]; pid[j+1] = t;
             }
         }
     }
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i-1] + bt[i-1];
+    }
 
-    int cur = 0;
     for (int i = 0; i < n; i++) {
-
-        if (cur < p[i].at)
-            cur = p[i].at;
-
-        p[i].wt = cur - p[i].at;
-        cur += p[i].bt;
-        p[i].tat = cur - p[i].at;
-    }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].original_index > p[j + 1].original_index) {
-                struct Process temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
-            }
-        }
+        tat[i] = wt[i] + bt[i];
     }
 
     double avgWT = 0, avgTAT = 0;
+    for (int i = 0; i < n; i++) {
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
+    avgWT /= n;
+    avgTAT /= n;
 
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d %d\n", p[i].pid, p[i].wt);
-        avgWT += p[i].wt;
-    }
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], wt[i]);
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d %d\n", p[i].pid, p[i].tat);
-        avgTAT += p[i].tat;
-    }
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], tat[i]);
 
-    printf("Average Waiting Time: %.2f\n", avgWT / n);
-    printf("Average Turnaround Time: %.2f\n", avgTAT / n);
+    printf("Average Waiting Time: %.2f\n", avgWT);
+    printf("Average Turnaround Time: %.2f\n", avgTAT);
 
     return 0;
 }
